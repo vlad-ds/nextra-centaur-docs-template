9945dfea4aa0afbf2a74a06f1420101c5e5dfa0c
vladgheorghe <vlad.proex@gmail.com>
2025-01-01 09:31:56 +0100

    undo next config change

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
-const generateContentFile = require('./scripts/generateContentFile');
-
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
 })
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // your existing config options...
-}
-
-// Wrap the config in a function that runs our script first
-module.exports = () => {
-  // Generate the content file before building
-  generateContentFile();
-  return withNextra(nextConfig);
-}
+module.exports = withNextra()
